414eb009b503264c1966a12089febffce9461a8b
joker <1491804727@qq.com>
2026-04-11 12:30:37 +0800

    更新

--- configs/_base_/datasets/hrsc.py
+++ configs/_base_/datasets/hrsc.py
@@ -28,8 +28,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         classwise=False,
